d9e3128562c53860c98a0ddbf1b35063785265f6
itamar600 <itamarzo0@gmail.com>
2020-12-01 20:39:55 +0200

    LoadActivity

---
+++ app/src/main/java/com/example/hamesakem/LoadActivity.java
@@ -0,0 +1,63 @@
+package com.example.hamesakem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class LoadActivity extends AppCompatActivity {
+
+    public void onClick(View v){
+        TextView course = (TextView)findViewById(R.id.tecourse);
+        TextView teacher = (TextView)findViewById(R.id.teteacher);
+        TextView year = (TextView)findViewById(R.id.teyear);
+        TextView simester = (TextView)findViewById(R.id.tesimester);
+        Toast.makeText(this, "course: "+ course.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    private void uploadFile() {
+        if (filePath != null) {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
+            StorageReference sRef = storageReference.child("uploads/"+ System.currentTimeMillis() + "." + getFileExtension(filePath));
+            sRef.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "File Uploaded ", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+//displaying the upload progress
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+                        }
+                    });
+        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+    }
+}
--- app/src/main/java/com/example/hamesakem/MainActivity.java
+++ app/src/main/java/com/example/hamesakem/MainActivity.java
@@ -2,10 +2,20 @@ package com.example.hamesakem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void onClick(View v)
+    {
+            Intent intent=new Intent(this,LoadActivity.class);
+            startActivity(intent);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
